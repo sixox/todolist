6808e35a525737ef9f8a252da6ae365fcdd45b94
sixox <moh3n.dehghani@gmail.com>
2023-07-31 08:57:24 +0330

    delete javascript for contact form sending and create model contact for it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  helper_method :contact_for_form
+
+  private
+
+  def contact_for_form
+    @contact ||= Contact.new
+  end
 end
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -944,43 +944,7 @@
         /////////////////////////////////////////////////
 
         //contact form processing
-        $('form.contact-form').on('submit', function (e) {
-            e.preventDefault();
-            var $form = $(this);
-            $($form).find('.contact-form-respond').remove();
-
-            //checking on empty values
-            $($form).find('[aria-required="true"], [required]').each(function (index) {
-                var $thisRequired = $(this);
-                if (!$thisRequired.val().length) {
-                    $thisRequired
-                        .addClass('invalid')
-                        .on('focus', function () {
-                            $thisRequired
-                                .removeClass('invalid');
-                        });
-                }
-            });
-            //if one of form fields is empty - exit
-            if ($form.find('[aria-required="true"], [required]').hasClass('invalid')) {
-                return;
-            }
-
-            //sending form data to PHP server if fields are not empty
-            var request = $form.serialize();
-            var ajax = jQuery.post("contact-form.php", request)
-                .done(function (data) {
-                    $($form).find('[type="submit"]').attr('disabled', false).parent().append('<div class="contact-form-respond color-main mt-20">' + data + '</div>');
-                    //cleaning form
-                    var $formErrors = $form.find('.form-errors');
-                    if (!$formErrors.length) {
-                        $form[0].reset();
-                    }
-                })
-                .fail(function (data) {
-                    $($form).find('[type="submit"]').attr('disabled', false).blur().parent().append('<div class="contact-form-respond color-main mt-20">Mail cannot be sent. You need PHP server to send mail.</div>');
-                })
-        });
+      
 
 
         //search modal
