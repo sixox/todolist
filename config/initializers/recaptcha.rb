741a3d1479fd9a21a46bb5d5ca5dfde5beccf8a0
sixox <moh3n.dehghani@gmail.com>
2023-07-31 09:53:58 +0330

    captcha chang version to v2

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,3 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key = '6Ld5BmwnAAAAAKJ-zfT9UMpHDT5s0LJxVGzyfc2G'
+  config.site_key = '6LcuIWwnAAAAANHvjVR7SO0UdejZdmYOz_vRQx6l'
+  config.secret_key = '6LcuIWwnAAAAAOCOJnW-gilxVlD9tWAyKG3ZuSHa'
 end
