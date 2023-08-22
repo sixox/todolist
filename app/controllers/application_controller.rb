96d4813fe163d3f8e6da02efeb2edcd4bc5413f2
sixox <moh3n.dehghani@gmail.com>
2023-08-22 11:39:52 +0330

    dynamic 3 news in small menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :set_menu_items
   helper_method :contact_for_form
 
   private
 
   def contact_for_form
     @contact ||= Contact.new
+  end
+
+
+  def set_menu_items
+    @menu_news_3 = Post.where(kind: "news").last(3)
   end
 end
