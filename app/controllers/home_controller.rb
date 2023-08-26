eff6312e59a1d5bc331ba13cc046c36fb599ac39
sixox <moh3n.dehghani@gmail.com>
2023-08-26 08:52:56 +0330

    add article section to homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
 	def index
 		@latest_news = Post.where(kind: 'news').order(created_at: :desc).limit(3)
 		@products = Product.all
+		@latest_articles = Post.where.not(kind: 'news').order(created_at: :desc).limit(8)
 	end
 end
