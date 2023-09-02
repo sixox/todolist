5ed069b8e53b43c65b1ee3affe171669d71037b8
sixox <moh3n.dehghani@gmail.com>
2023-09-02 11:35:54 +0330

    article homepage section with sidebar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,6 @@ class HomeController < ApplicationController
 		@latest_news = Post.where(kind: 'news').order(created_at: :desc).limit(3)
 		@products = Product.all
 		@latest_articles = Post.where.not(kind: 'news').order(created_at: :desc).limit(9)
+		@application_posts = Post.where(kind: 'application')
 	end
 end
