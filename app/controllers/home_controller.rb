9943faf96f0b9af3e934da74aace2dc88e9976b7
sixox <moh3n.dehghani@gmail.com>
2023-09-02 12:14:20 +0330

    edit article hom sidebar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 	def index
 		@latest_news = Post.where(kind: 'news').order(created_at: :desc).limit(3)
 		@products = Product.all
-		@latest_articles = Post.where.not(kind: 'news').order(created_at: :desc).limit(9)
+		@latest_articles = Post.where.not(kind: 'news').order(created_at: :desc).limit(14)
 		@application_posts = Post.where(kind: 'application')
 	end
 end
