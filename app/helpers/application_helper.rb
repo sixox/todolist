de223b8b8d869532835ba6599fb28f3bdf0dcac7
sixox <moh3n.dehghani@gmail.com>
2024-04-06 14:47:25 +0330

    friendly url02-menu link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def safe_path_for(model, slug, default: "#")
+    record = model.friendly.find(slug) rescue nil
+    return default unless record
+
+    case model.name
+    when 'Product'
+      product_path(record)
+    when 'Grade'
+      grade_path(record)
+    else
+      "#"
+    end
+  end
 end
